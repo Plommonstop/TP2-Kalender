06ff2ef976a1e03dc36bb09acd8dba0cb896f483
Sammi Mughal <zalamimughal@outlook.com>
2018-12-11 12:55:20 +0100

    har lagt till html kog

--- logout.php
+++ logout.php
@@ -1,3 +1,4 @@
+<!DOCKTYPE <!DOCTYPE html>
 <?php
 session_start();
 unset($_SESSION['account']);
@@ -6,3 +7,16 @@ session_destroy();
 header("Location: Loggain.php");
 exit;
 ?>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="style.css" />
+    <script src=""></script>
+</head>
+<body>
+    
+</body>
+</html>>
