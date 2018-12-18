d49f26ba1581b62c4a421266d4d9b1f0c9160b6f
plommonstop <arkel.dagsberg@learnet.se>
2018-12-18 11:11:25 +0100

    jk

--- Vy.php
+++ Vy.php
@@ -17,6 +17,24 @@
             $_SESSION["account"] = $account["accountID"];
     }
 
+    require_once ("mycurl.php");
+    if(isset($_POST["name"]) && isset($_POST["starttime"])){
+    $name = $_POST["name"];
+    $location = $_POST["location"];
+    $description = $_POST["description"];
+    $starttime = $_POST["starttime"];
+    $endtime = $_POST["endtime"];
+    $forCalendarID = $_POST["forCalendarID"];
+    $response = myCurl::execute_curl("http://10.130.216.144/~theprovider/calendar/php/create-activity.php",
+    [
+        "name"=>$name,
+        "location"=>$location,
+        "description"=>$description,
+        "starttime"=>$starttime,
+        "endtime"=>$endtime,
+        "forCalendarID"=>$forCalendarID
+    ]);
+    }
 ?>
 
 <html>
