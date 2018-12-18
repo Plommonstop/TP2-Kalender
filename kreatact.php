7961af8bf7b2d63bad5e62d89dd739cdc221a9da
plommonstop <arkel.dagsberg@learnet.se>
2018-12-18 13:01:26 +0100

    skd

--- kreatact.php
+++ kreatact.php
@@ -1,23 +1,24 @@
 <?php
 
 session_start();
-require_once ("mycurl.php");
-if(isset($_POST["name"]) && isset($_POST["starttime"])){
-$name = $_POST["name"];
-$location = $_POST["location"];
-$description = $_POST["description"];
-$starttime = $_POST["starttime"];
-$endtime = $_POST["endtime"];
-$forCalendarID = $_POST["forCalendarID"];
-$response = myCurl::execute_curl("http://10.130.216.144/~theprovider/calendar/php/create-activity.php",
-[
-    "name"=>$name,
-    "location"=>$location,
-    "description"=>$description,
-    "starttime"=>$starttime,
-    "endtime"=>$endtime,
-    "forCalendarID"=>$forCalendarID
-]);
-}
-    var_dump ($result);
+    require_once ("mycurl.php");
+    $name = "hej";
+    $location = "hejj";
+    $description = "hejjj";
+    $starttime = "2018-13-18 13:00:00";
+    $endtime = "2018-13-18 15:00:00";
+    $activity="62";
+    $response = myCurl::execute_curl("http://10.130.216.144/~theprovider/calendar/php/create-activity.php",
+    [
+        "name"=>$name,
+        "location"=>$location,
+        "description"=>$description,
+        "starttime"=>$starttime,
+        "endtime"=>$endtime,
+        "token"=>$_SESSION["token"],
+        "accountID"=>$_SESSION["account"],
+        "activity"=>$activity
+    ]);
+
+    var_dump($response);
 ?>
