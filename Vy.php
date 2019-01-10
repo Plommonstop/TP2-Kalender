66c4956f02dbaeb33308071491067bd34459c659
Sammi Mughal <zalamimughal@outlook.com>
2019-01-10 15:22:38 +0100

    lite mer text

--- Vy.php
+++ Vy.php
@@ -67,7 +67,7 @@
             
 </div>
 <div id="container">
-<textarea id="aktivitet" rows="4">Här visas dina aktivitet</textarea>
+<textarea id="aktivitet" rows="4">Aktivitet</textarea>
 
 </div>
 
